88aff5082d773d77283c0c74cb8d83f0ae1b2c32
Ellie S <asila527@gmail.com>
2020-10-23 15:14:12 -0700

    add files

---
+++ methods.js
@@ -0,0 +1,11 @@
+let square = {
+    area: function(side) {
+        return side * side;
+    },
+    perimeter: function(side) {
+        return side * 4;
+    }
+}
+
+square.area(10);
+square.perimeter(10);
